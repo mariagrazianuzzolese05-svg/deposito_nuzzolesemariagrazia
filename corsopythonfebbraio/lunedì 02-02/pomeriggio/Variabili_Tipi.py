446dd3c27e1a0213187cebfa78bc75679b994336
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-02 17:54:15 +0100

    Add files via upload

---
+++ corsopythonfebbraio/lunedì 02-02/pomeriggio/Variabili_Tipi.py
@@ -0,0 +1,67 @@
+#prova print e variabili
+nome = "mariagrazia"
+numero = 20
+print(nome, numero)
+print("questo è il mio nome:" , nome, "questo è il mio numero:" , numero)
+
+#ogni variabile separata da virgole
+
+name = "alice"
+età = int(input("inserisci la tua età:"))
+sesso = input("di che sesso sei?")
+print("ciao, " + name + "!")
+
+#prova operatori
+x=1
+y=2
+print(x + y)
+print(x - y)
+print(x / y)
+print(x ** y)
+
+a = "mirko"
+print("ciao " + a) 
+
+# prova concatenazione stringa
+b = "ciao"
+print(b + " " + a)
+
+#prova commenti su più linee
+""" 
+print(2 + 3)
+ """
+
+#prova float
+c = 1
+d = 0.2
+print(c - d)
+print(int(c - d))
+
+#prova stringa
+s =input("parola:")
+print(s[0])
+
+#prova metodi
+e = "Ciao, Mondo!"
+print(len(e))
+print(e.upper())
+print(e.lower())
+#ricorda virgola
+print(e.split(','))
+print(e.replace('Mondo' , 'universo'))
+
+#prova char
+carattere = "a"
+
+#prova booleans
+booleanT = True
+booleanF = int(False)
+print( booleanF )
+print( 5 > 10 )
+
+#operatori di confronto
+g = 5
+h = 10
+print(x == y)
+print(x != y)
+print(x < y)
