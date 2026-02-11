b2c478752f499e2cf382b200a50d654a1f43ec9f
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-11 16:34:02 +0100

    banca

--- corsopythonfebbraio/mercoledì 11-02/pomeriggio/es_incaps-banca.py
+++ corsopythonfebbraio/mercoledì 11-02/pomeriggio/es_incaps-banca.py
@@ -16,3 +16,62 @@ applicando validazioni appropriate (e.g., il titolare deve essere una stringa no
 '''ss Utente:
 class Cliente(Utente):
 class Admin(Utente):'''
+
+'''Classe ContoBancario:
+Attributi privati:
+__titolare (stringa che rappresenta il nome del titolare del conto)
+__saldo (decimale che rappresenta il saldo del conto)'''
+
+class Utente:
+    def __init__(self,nometitolare,saldo):
+        self.__nometitolare=nometitolare
+        self.__saldo=saldo
+    def viewprivate(self):
+        get_nometitolareconto()
+        get__saldo()
+
+class Admin(Utente):
+    def __init__(self, nometitolare, saldo):
+        super().__init__(nometitolare, saldo)()
+    def viewprivate(self):
+        return super().viewprivate()
+    
+class Cliente(Utente):
+    def __init__(self, nometitolare, saldo):
+        super().__init__(nometitolare, saldo)
+    def viewprivate(self):
+        return super().viewprivate()
+    def modifyprivate(self):
+        if _nometitolareconto is None:
+            x=input('modificare in')
+            set_nometitolareconto(x)
+            print('modificato in',__nometitolareconto)
+    
+class ContoBancario:
+    def __init__(self,nometitolareconto):
+        self.__nometitolareconto=nometitolareconto
+
+    def deposito(self):
+        importo=input('di quanto aggiugnere')
+        if importo>0:
+         self.__saldo+=importo
+         print(self.__saldo,'totale')
+        else:
+            print('non valido')
+    def preleva(self):
+        importo=input('di quanto togliere')
+        if importo>0,self.__saldo>0
+            self.__saldo-=importo
+            print(self.__saldo)
+        else:
+            print('non valido')
+    def visualizza(self):
+         print(self.__saldo)
+
+cliente1=Cliente('mariagrazia',99999)
+admin1=Admin('paolo',100)
+conto=ContoBancario(cliente1)
+
+        
+
+    
