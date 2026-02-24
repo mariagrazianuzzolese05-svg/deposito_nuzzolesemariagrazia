732b61b5d082131052f2757b2818c1d760daebe9
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-24 09:06:58 +0100

    c

--- corsopythonfebbraio/lunedì 23-02/pomeriggio/esempinumpy.py
+++ corsopythonfebbraio/lunedì 23-02/pomeriggio/esempinumpy.py
@@ -68,4 +68,36 @@ print(arr_2d[:, 1:3])  # Output: [[ 2  3]
 print(arr_2d[1:3, 1:3])  # Output: [[ 6  7]
 
                         #          [10 11]]
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+# Slicing di base
+print(arr[2:7])  # Output: [2 3 4 5 6]
+
+# Slicing con passo
+print(arr[1:8:2])  # Output: [1 3 5 7]
+
+# Omettere start e stop
+print(arr[:5])  # Output: [0 1 2 3 4]
+print(arr[5:])  # Output: [5 6 7 8 9]
+
+# Utilizzare indici negativi
+print(arr[-5:])  # Output: [5 6 7 8 9]
+print(arr[:-5])  # Output: [0 1 2 3 4]
+
+#fancy index
+arr = np.array([10, 20, 30, 40, 50])
+# Utilizzo di un array di indici
+indices = np.array([1, 3])
+print(arr[indices])  # Output: [20 40]
+# Utilizzo di una lista di indici
+indices = [0, 2, 4]
+print(arr[indices])  # Output: [10 30 50]
+
+import random
+matrice=np.random.randint(1, 101, size=(6, 6))
+#matrice_invertita = sotto_matrice[::-1, :]
+#diagonale = np.diag(matrice_invertita)
+
+
 
