37ae7095880f27be5612184ac71bbfea442e7a72
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-25 11:32:41 +0100

    b

---
+++ corsopythonfebbraio/mercoledì 25-02/mattina/espandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+# Percorso del file CSV
+file_path = 'vendite.csv'
+# Caricamento dei dati nel DataFrame
+df = pd.read_csv(file_path)
+#le prime righe del DataFrame per confermare 
+print(df.head())
